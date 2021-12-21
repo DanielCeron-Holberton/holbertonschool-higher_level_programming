85d4cf1ce73db0c71d7377a9f4644d3f162506f4
DanielCeron-Holberton <3403@holbertonschool.com>
2021-12-21 16:43:03 -0500

    [FIX] - next pointer for the new node assignment fixed

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -25,11 +25,11 @@ listint_t *insert_node(listint_t **head, int number)
 		*head = new;
 	else
 	{
-		while ((current->next != NULL) && (current->next->n < number))
+		while ((current->next != NULL) && (current->next->n <= number))
 		{
 			current = current->next;
-			new->next = current->next;
 		}
+		new->next = current->next;
 		current->next = new;
 	}
 
