7fcb6d92b15505fc2839893d9733edb9dd285bb6
DanielCeron-Holberton <3403@holbertonschool.com>
2022-01-03 18:00:00 -0500

    [ADD] - Task #13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * is_palindrome - Verify if the linked list is palindrome
+ *
+ * @head: Input linked list
+ * Return: 1 if it's palindrome 0 if it's not
+ */
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *current;
+	int sum = 0;
+
+	current = *head;
+
+	while (current->next != NULL)
+	{
+		if (current->n != current->next->n)
+			sum += current->n;
+		else
+		{
+			sum += current->n;
+			break;
+		}
+		current = current->next;
+	}
+
+	while (current->next != NULL)
+	{
+		current = current->next;
+		sum -= current->n;
+	}
+	if (sum == 0)
+		return (1);
+	return (0);
+}
