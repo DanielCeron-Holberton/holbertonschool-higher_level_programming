8a9cc1a5f90a32f97241b5219024e100eca74ba8
DanielCeron-Holberton <3403@holbertonschool.com>
2022-01-05 17:11:45 -0500

    [FIX] - Changed for use max function

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not a_dictionary:
-        return None
-    values_list = sorted(list(a_dictionary.values()))
-    bigger_value = values_list[-1]
-    for key in a_dictionary:
-        value_dict = a_dictionary.get(key)
-        if value_dict == bigger_value:
-            print(key)
-            return key
+    if a_dictionary:
+        return max(a_dictionary, key=a_dictionary.get)
