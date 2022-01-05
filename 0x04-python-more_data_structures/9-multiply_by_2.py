c32f5ea451e403d952fb99793ff5ab62eb5ccd6b
DanielCeron-Holberton <3403@holbertonschool.com>
2022-01-05 14:48:57 -0500

    [ADD] - Task #9

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    new_dictionary = {}    
+    for key in a_dictionary:
+        value_dict = a_dictionary.get(key) * 2
+        new_dictionary.update({key: value_dict})
+    return new_dictionary
