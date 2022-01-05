abbfeaa1da8562e16c224335958a0e4a7208d898
DanielCeron-Holberton <3403@holbertonschool.com>
2022-01-05 14:52:11 -0500

    [FIX] - Style code issue

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dictionary = {}    
+    new_dictionary = {}
     for key in a_dictionary:
         value_dict = a_dictionary.get(key) * 2
         new_dictionary.update({key: value_dict})
