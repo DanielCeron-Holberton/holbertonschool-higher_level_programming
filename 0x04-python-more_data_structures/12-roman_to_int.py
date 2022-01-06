93fbe4dbe257733ea597cbbfe142b7200cfcc959
DanielCeron-Holberton <3403@holbertonschool.com>
2022-01-05 20:23:44 -0500

    [FIX] - Substracion on IV fixed

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     if not (isinstance(roman_string, str)):
         return None
-    value_integer = 0
+    value_integer = []
     dict_new = {
         'I': 1,
         'V': 5,
@@ -12,12 +12,16 @@ def roman_to_int(roman_string):
         'D': 500,
         'M': 1000,
     }
+
     for letter in roman_string:
         for key in dict_new:
             if letter == key:
-                if value_integer != 0 and value_integer < dict_new.get(key):
-                    value_integer = dict_new.get(key) - value_integer
+                if value_integer and value_integer[-1] < dict_new.get(key):
+                    current_value = dict_new.get(key)
+                    value_integer.append(current_value - value_integer[-1])
+                    del(value_integer[-2])
                 else:
-                    value_integer += dict_new.get(key)
+                    current_value = dict_new.get(key)
+                    value_integer.append(current_value)
 
-    return value_integer
+    return sum(value_integer)
