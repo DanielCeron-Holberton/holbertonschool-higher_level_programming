c47e09258903a23a3f2f835bbfcf82c13b321cd1
DanielCeron-Holberton <3403@holbertonschool.com>
2022-01-17 17:22:54 -0500

    [ADD] Task #3

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+from typing import final
+
+
+def safe_print_division(a, b):
+    try:
+        result = a/b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+
+    return result
