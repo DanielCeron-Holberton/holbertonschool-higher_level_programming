d48142cbdbff79e6e51ab05d12752f22f9dfbb4f
DanielCeron-Holberton <3403@holbertonschool.com>
2022-01-17 16:59:11 -0500

    [ADD] - Task #2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    a = 0
+    b = 0
+    while (x > a):
+        try:
+            if type(my_list[a]) == int:
+                print("{:d}".format(my_list[a]), end='')
+                b = b + 1
+        except (ValueError, TypeError):
+            pass
+        a = a + 1
+    print()
+    return b
