edc45262ed446287bbc9bae42d4bce11fc259950
DanielCeron-Holberton <3403@holbertonschool.com>
2022-01-17 13:28:44 -0500

    [FIX] - new line print issue

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,10 +7,8 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[i], end='')
             i += 1
-            if x == 1:
-                print()
             x -= 1
         except IndexError:
-            print()
             break
+    print()
     return i
