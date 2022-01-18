d5b0fac999f90c5923df2f0e41e0db52ca851cff
Daniel Ceron <danielceron@pop-os.localdomain>
2022-01-18 17:52:31 -0500

    [ADD]- Task #1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Create new class and asign it a private instance attrb"""
+
+
+class Square:
+    """Square Class declarated and Private instance attribute: size
+    initiated.
+    """
+
+    def __init__(self, size):
+        self.__size = size
