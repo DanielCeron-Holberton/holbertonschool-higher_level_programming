0dc8c039787566c557987474da95ba0d79873062
Daniel Ceron <danielceron@pop-os.localdomain>
2022-01-25 18:56:06 -0500

    [FIX] - HotFix

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-def magic_string():
-    magic_string.n_list += ["BestSchool"]
-    return ", ".join(magic_string.n_list)
-magic_string.n_list = []
+def magic_string(n_list=[]):
+    n_list.append("BestSchool")
+    return ", ".join(n_list)
