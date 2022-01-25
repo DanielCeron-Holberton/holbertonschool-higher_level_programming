300ffa9ab48967b06dff5197bddbdabca78bff8f
Daniel Ceron <danielceron@pop-os.localdomain>
2022-01-25 13:47:22 -0500

    [ADD] - New Project and tasks with test cases

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+def matrix_divided(matrix, div):
+
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    if not isinstance(matrix, list):
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
+
+    size = []
+
+    for lst in matrix:
+        if not isinstance(lst, list):
+            raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats")
+        size.append(len(lst))
+    size = set(size)
+    if not (len(size) == 1):
+        raise TypeError("Each row of the matrix must have the same size")
+
+    new_matrix = []
+    for row in matrix:
+        templist = []
+        for element in row:
+            if not isinstance(element, (int, float)):
+                raise TypeError(
+                    "matrix must be a matrix (list of lists) of integers/floats")
+            templist.append(round(element / div, 2))
+        new_matrix.append(templist)
+
+    return new_matrix
