4a02e10cec33368845e48ab1b2e234f5bf67b06d
Daniel Ceron <danielceron@pop-os.localdomain>
2022-01-25 18:37:54 -0500

    [ADD] - New Project and all mandatory tasks

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,4 @@
+def magic_string():
+    magic_string.n_list += ["BestSchool"]
+    return ", ".join(magic_string.n_list)
+magic_string.n_list = []
---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module to copy a list"""
+
+
+def copy_list(l):
+    """Copy list function"""
+
+    return l[:]
