b7197c9fb399890add1a61104e16a2878b0fee3e
Daniel Ceron <danielceron@pop-os.localdomain>
2022-01-31 20:23:52 -0500

    [FIX] - validator fixed

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -8,5 +8,6 @@ class Square(Rectangle):
     """Creates a new subclass"""
 
     def __init__(self, size):
+        self.integer.validator("size", size)
         super().__init__(size, size)
         self.__size = size
