a581e077f59a33bbe924345745ec53279f0d62a6
Daniel Ceron <danielceron@pop-os.localdomain>
2022-01-31 19:18:36 -0500

    [FIX] - Code Style Issue / Documentation added

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""This module compares an object with a class"""
+
 
 def is_same_class(obj, a_class):
+    """
+    If obj is exactly class returns 
+    true else returns false
+    """
     if(type(obj) is a_class):
         return True
     else:
