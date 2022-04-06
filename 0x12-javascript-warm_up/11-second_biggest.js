3a351572d35e4772b5f56b59a6031cb1fe4fb082
DanielC <3403@holbertonschool.com>
2022-04-06 00:13:35 -0500

    [FIX] - Code Functionality

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,5 +2,14 @@
 
 //const que hace aqui joven aún entre las verdes ramas http://sedemargaritas.blogspot.com/2012/11/un-poema-de-epifanio-mejia.html
 
-const { argv } = require('process')
+const { argv } = require('process');
 
+if (!argv[2] || !argv[3]) {
+    console.log(0)
+}
+else {
+    let sortedArgs = argv.sort();
+    sortedArgs = argv.reverse();
+
+    console.log(sortedArgs[1]);
+}
