5d8093d1ebd1a5121ee78dbf90e8e3fc74d325e4
DanielCeron <3403@holbertonschool.com>
2022-04-13 01:23:26 -0500

    [FIX] - Style and API read issue

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,30 +1,24 @@
 #!/usr/bin/node
 
-
-
 const { argv } = require('process');
 
 const request = require('request');
 
 if (argv[2]) {
-    let counter = 0;
-    request(argv[2], function (error, response, body) {
-        let toFind = 'https://swapi-api.hbtn.io/api/people/18/';
-        if (error)
-            console.log('error:', error);
-        else {
-            const characterID = JSON.parse(body).results;
-            for (let index = 0; index < characterID.length; index++) {
-                const listCharacters = Object.values(characterID[index].characters);
-                listCharacters.forEach(element => {
-                    if (element === toFind) {
-                        counter += 1;
-                    }
-                });
-                
-
-            }
-            console.log(counter);
-        }
-    })
+  let counter = 0;
+  request(argv[2], function (error, response, body) {
+    const toFind = '18/';
+    if (error) { console.log('error:', error); } else {
+      const characterID = JSON.parse(body).results;
+      for (let index = 0; index < characterID.length; index++) {
+        const listCharacters = Object.values(characterID[index].characters);
+        listCharacters.forEach(element => {
+          if (element.slice(-3) === toFind) {
+            counter += 1;
+          }
+        });
+      }
+      console.log(counter);
+    }
+  });
 }
