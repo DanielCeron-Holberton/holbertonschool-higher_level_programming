e29b3de5fbebd956fe753cdb10a7055cb6ce0c79
Daniel <3403@holbertonschool.com>
2022-05-10 10:16:43 -0500

    [FIX] - Code Style issue

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,11 +1,9 @@
-#!/usr/bin/python3 
+#!/usr/bin/python3
 """ Module Filter states by user input"""
 
 
 import MySQLdb
 from sys import argv
-
-
 
 if len(argv) <= 4:
     print("Not enough args")
@@ -15,9 +13,9 @@ db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3])
 
 cur = db.cursor()
 
-query = "SELECT * FROM states WHERE name = '{:s}' ORDER BY id ASC".format(argv[4])
-
-cur.execute(query)
+cur.execute("""SELECT * FROM states
+                WHERE name = '{:s}'
+                ORDER BY id ASC""".format(argv[4]))
 
 rows = cur.fetchall()
 
@@ -30,4 +28,3 @@ db.close()
 
 if __name__ == "__main__":
     pass
-
