3d5eb9f82cab4f73e161bd58035840672abddf4c
Daniel <3403@holbertonschool.com>
2022-05-12 16:09:03 -0500

    [ADD] - Task #13 Deletes an element

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Module that deletes an element from the database
+"""
+
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]
+    ), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(engine)
+    session = Session()
+
+    elements_to_delete = session.query(State).filter(State.name.contains('a'))
+
+    for element in elements_to_delete:
+        session.delete(element)
+
+    session.commit()
+    session.close()
