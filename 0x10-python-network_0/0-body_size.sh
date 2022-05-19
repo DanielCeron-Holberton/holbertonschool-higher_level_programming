ee60fe88a7b1d4ea08efc4365a5fd35e7dcee1a7
Daniel <3403@holbertonschool.com>
2022-05-18 21:11:23 -0500

    [FIX] - HotFix

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -5,4 +5,4 @@ if [ $# -lt 1 ]; then
 	echo "Not enough args"
 fi
 
-curl -sI {"$2"} | grep -i Content-Length | cut -d ":" -f 2 | cut -d " " -f2
+curl -sI "$2" | grep -i Content-Length | cut -d ":" -f 2 | cut -d " " -f2
