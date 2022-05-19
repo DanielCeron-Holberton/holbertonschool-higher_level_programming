af4dbb698b956835bace4bf26145dfe1efdb1efa
Daniel <3403@holbertonschool.com>
2022-05-18 21:10:35 -0500

    [FIX] - HotFix

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/bash
 # Used curl to get the body size
 
-
-if [$# -lt 2]:
+if [ $# -lt 1 ]; then
 	echo "Not enough args"
-	
+fi
 
-
-curl -sI {$2} | grep -i Content-Length | cut -d ":" -f 2 | cut -d " " -f2
+curl -sI {"$2"} | grep -i Content-Length | cut -d ":" -f 2 | cut -d " " -f2
