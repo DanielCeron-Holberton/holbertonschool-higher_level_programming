4acbe504d8f5a864ea46974aeb0900b1d77cb154
Daniel <3403@holbertonschool.com>
2022-05-18 22:52:39 -0500

    [ADD] - Task #6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Finds the peak
+
+
+def find_peak(list_of_integers):
+    """find_peak"""
+
+    if list_of_integers:
+        list_of_integers.sort()
+        return list_of_integers[-1]
