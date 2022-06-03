b4e5476426c71e23ab64405898a3408e8fc66d47
Daniel <3403@holbertonschool.com>
2022-06-03 01:42:22 -0500

    [FIX] - Doc Issue

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-"""Module that fetch an URL"""
+"""
+Write a Python script that takes in a URL, sends a request to the URL
+and displays the value of the X-Request-Id variable
+found in the header of the response.
+"""
 
 import urllib.request
 from sys import argv
 
 
-request = urllib.request.Request(argv[1])
-with urllib.request.urlopen(request) as response:
-    print(response.headers.get('X-Request-Id'))
+if __name__ == "__main__":
+    req = urllib.request.Request(argv[1])
+    with urllib.request.urlopen(req) as response:
+        print(response.headers.get('x-Request-Id'))
