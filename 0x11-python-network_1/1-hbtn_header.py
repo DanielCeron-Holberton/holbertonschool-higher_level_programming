eb2c01b3e020d3acb6fdf17f367e61babe1fca5e
Daniel <3403@holbertonschool.com>
2022-06-03 01:37:19 -0500

    [FIX] - Doc Issue

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
 Takes in a URL, sends a request to the URL
-and displays the value of the X-Request-Id Header.
+and displays the value of the X-Request-Id Header
+in the console
 """
 
 
