b1a53fbb2f15e77228640c5ae5ac97100a8424c0
Daniel <3403@holbertonschool.com>
2022-06-03 01:32:38 -0500

    [ADD] - Task 1

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module that takes in a URL, sends a request to the URL"""
+
+
+from sys import argv
+import urllib.request
+
+url_var = argv[1]
+
+request = urllib.request.Request(url_var)
+with urllib.request.urlopen(request) as response:
+    print(response.headers.get('X-Request-Id'))
